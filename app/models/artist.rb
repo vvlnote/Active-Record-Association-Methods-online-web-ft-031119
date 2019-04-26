0816e43499d7a0bb64102adcda1d4500b53e76e0
Joy Lin <vvlnote@hotmail.com>
2019-04-26 04:44:38 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,23 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    self.songs.first.genre
   end
 
   def song_count
     #return the number of songs associated with the artist
+    self.songs.length
   end
 
   def genre_count
+    genre_arr = []
     #return the number of genres associated with the artist
+    self.songs.each do |song|
+      if genre_arr.find_index(song.genre_id) == nil
+        genre_arr << song.genre_id
+      end
+      puts genre_arr
+    end
+    genre_arr.length
   end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,28 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    self.songs.length
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    artist_arr = []
+    self.songs.each do |song|
+      if artist_arr.find_index(song.artist_id) == nil
+        artist_arr << song.artist_id
+      end
+    end
+    artist_arr.length
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    artist_names = []
+    self.songs.each do |song|
+      if artist_names.find_index(song.artist.name) == nil
+        artist_names << song.artist.name
+      end
+    end
+    artist_names
   end
 end
